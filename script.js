2ef4139f8d9d87e7a6586a12b559a363e524531f
Muhammad Ali <m.ali@aptechnn2.edu>
2026-01-29 14:08:10 +0500

    updated using bootstrap5 and jquery

--- script.js
+++ script.js
@@ -7,151 +7,96 @@ let products = [
 ];
 
 let cart = [];
-let productList = document.getElementById("productList");
 
 function renderProducts() {
-  productList.innerHTML = "";
+  $("#productList").empty();
 
-  for (var i = 0; i < products.length; i++) {
-    var product = products[i];
-
-    var card = document.createElement("div");
-    card.className = "product-card";
-
-    var img = document.createElement("img");
-    img.src = product.image;
-    img.alt = product.name;
-
-    var h3 = document.createElement("h3");
-    h3.innerText = product.name;
-
-    var p = document.createElement("p");
-    p.innerText = product.price;
-
-    var btn = document.createElement("button");
-    btn.innerText = "Add To Cart";
-
-    // simpler way to attach onclick
-    btn.onclick = (function (id) {
-      return function () {
-        addToCart(id);
-      };
-    })(product.id);
-
-    card.appendChild(img);
-    card.appendChild(h3);
-    card.appendChild(p);
-    card.appendChild(btn);
-
-    productList.appendChild(card);
-  }
+  $.each(products, function (_, p) {
+    $("#productList").append(`
+      <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+        <div class="card h-100 shadow-sm">
+          <img src="${p.image}" class="card-img-top" style="height:150px;object-fit:cover">
+          <div class="card-body text-center">
+            <h5>${p.name}</h5>
+            <p class="text-primary fw-bold">${p.price}</p>
+            <button class="btn btn-primary btn-sm addBtn" data-id="${p.id}">
+              Add To Cart
+            </button>
+          </div>
+        </div>
+      </div>
+    `);
+  });
 }
+
+$(document).on("click", ".addBtn", function () {
+  let id = $(this).data("id");
+  let item = cart.find(i => i.id === id);
+
+  if (item) {
+    item.qty++;
+  } else {
+    let p = products.find(p => p.id === id);
+    cart.push({ ...p, qty: 1 });
+  }
+});
+
+function renderCart() {
+  let subtotal = 0;
+  $("#cartBody").empty();
+
+  $.each(cart, function (i, item) {
+    let total = item.price * item.qty;
+    subtotal += total;
+
+    $("#cartBody").append(`
+      <tr>
+        <td>${item.name}</td>
+        <td>
+          <button class="btn btn-sm btn-secondary qtyMinus" data-i="${i}">-</button>
+          ${item.qty}
+          <button class="btn btn-sm btn-secondary qtyPlus" data-i="${i}">+</button>
+        </td>
+        <td>${item.price}</td>
+        <td>${total}</td>
+        <td>
+          <button class="btn btn-sm btn-primary removeItem" data-i="${i}">X</button>
+        </td>
+      </tr>
+    `);
+  });
+
+  let tax = subtotal * 0.05;
+  $("#subtotal").text("Subtotal: " + subtotal);
+  $("#tax").text("Tax (5%): " + tax);
+  $("#grandTotal").text("Grand Total: " + (subtotal + tax));
+}
+
+$(document).on("click", ".qtyPlus", function () {
+  cart[$(this).data("i")].qty++;
+  renderCart();
+});
+
+$(document).on("click", ".qtyMinus", function () {
+  let i = $(this).data("i");
+  cart[i].qty > 1 ? cart[i].qty-- : cart.splice(i, 1);
+  renderCart();
+});
+
+$(document).on("click", ".removeItem", function () {
+  cart.splice($(this).data("i"), 1);
+  renderCart();
+});
+
+$("#showCartBtn").click(function () {
+  renderCart();
+  new bootstrap.Modal("#cartModal").show();
+});
+
+$("#clearCartBtn").click(function () {
+  cart = [];
+  renderCart();
+  alert("Cart cleared");
+});
 
 renderProducts();
-
-function addToCart(id) {
-  var foundProduct = null;
-
-  for (var i = 0; i < products.length; i++) {
-    if (products[i].id === id) {
-      foundProduct = products[i];
-      break;
-    }
-  }
-
-  var already = null;
-  for (var j = 0; j < cart.length; j++) {
-    if (cart[j].id === id) {
-      already = cart[j];
-      break;
-    }
-  }
-
-  if (already != null) {
-    already.qty = already.qty + 1;
-  } else {
-    cart.push({
-      id: foundProduct.id,
-      name: foundProduct.name,
-      price: foundProduct.price,
-      image: foundProduct.image,
-      qty: 1
-    });
-  }
-}
-
-var showBillBtn = document.getElementById("showBillBtn");
-var clearBillBtn = document.getElementById("clearBillBtn");
-var billModal = document.getElementById("billModal");
-var closeModal = document.getElementById("closeModal");
-
-showBillBtn.onclick = function () {
-  billModal.classList.remove("hidden");
-  renderBill();
-};
-
-clearBillBtn.onclick = function () {
-  cart = [];
-  renderBill();
-  alert("Cart has been cleared!");
-};
-
-closeModal.onclick = function () {
-  billModal.classList.add("hidden");
-};
-
-function renderBill() {
-  var billBody = document.getElementById("billBody");
-  billBody.innerHTML = "";
-
-  var subtotal = 0;
-
-  for (var i = 0; i < cart.length; i++) {
-    var item = cart[i];
-    var total = item.price * item.qty;
-    subtotal = subtotal + total;
-
-    var row = document.createElement("tr");
-    row.innerHTML = "<td>" + item.name + "</td>" +
-      "<td>" +
-      "<button onclick='decreaseQty(" + i + ")'>-</button> " +
-      item.qty +
-      " <button onclick='increaseQty(" + i + ")'>+</button>" +
-      "</td>" +
-      "<td>" + item.price + "</td>" +
-      "<td>" + total + "</td>" +
-      "<td><button onclick='removeItem(" + i + ")'>X</button></td>";
-
-    billBody.appendChild(row);
-  }
-
-  var tax = subtotal * 0.05;
-  var grandTotal = subtotal + tax;
-
-  document.getElementById("subtotal").innerText = "Subtotal: " + subtotal;
-  document.getElementById("tax").innerText = "Tax (5%): " + tax;
-  document.getElementById("grandTotal").innerText = "Grand Total: " + grandTotal;
-}
-
-function increaseQty(index) {
-  cart[index].qty = cart[index].qty + 1;
-  renderBill();
-}
-
-function decreaseQty(index) {
-  if (cart[index].qty > 1) {
-    cart[index].qty = cart[index].qty - 1;
-  } else {
-    cart.splice(index, 1);
-  }
-  renderBill();
-}
-
-function removeItem(index) {
-  cart.splice(index, 1);
-  renderBill();
-}
-
-document.getElementById("printBillBtn").onclick = function () {
-  window.print();
-};
